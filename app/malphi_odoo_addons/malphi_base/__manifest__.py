053d52bd840c945e27751090d963ecc29bc669c0
Lynard Salingujay <lynardsalingujay@gmail.com>
2018-06-01 10:22:42 +0000

    added configuration and view files

---
+++ app/malphi_odoo_addons/malphi_base/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "malphi_base",
+
+    'summary': """MalPhi base configuration""",
+
+    'description': """
+        MalPhi module for managing trainings:
+            - training courses
+            - training sessions
+            - attendees registration
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'templates.xml',
+        'views/malphi.xml'
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+
+    'application' : True,
+}
